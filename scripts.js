412cc07434652ae8c85d383a0bb59c741f01349f
KingAshtor <Ashtonsisson@gmail.com>
2021-01-15 02:35:40 -0500

    register.html

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@ function getEleId(input) {
   return document.getElementById(input);
 }
 
+//simplifies getElementsByTagName (got tired of typeing it)
+function getEleTag(input) {
+  return document.getElementsByTagName(input);
+}
+
+//simplifies getElementsByTagName (got tired of typeing it)
+function getEleName(input) {
+  return document.getElementsByName(input);
+}
+
 
 
 //code for the calorie calculator
@@ -47,3 +57,65 @@ function metCalc(input) {
 
 
 //code for the form
+function formCheck() {
+  let errs = [];
+  let radio = getEleId("bicycle");
+  let radio1 = getEleId("bicycle1");
+  let fName = getEleId("fName").value;
+  let lName = getEleId("lName").value;
+  let email = getEleId("email").value;
+
+  if (fName == "") {
+    errs.push("Please enter your First Name")
+  }
+
+  if (lName == "") {
+    errs.push("Please enter your Last Name")
+  }
+
+  if (email == "") {
+    errs.push("Please enter your email")
+  }
+
+  if (checkBoxRoundUp() < 1) {
+    errs.push("Please Select at least one check Box");
+  }
+
+  if (radio.checked || radio1.checked) {} else {
+    errs.push("Please select wether you will rent a bike or bring your own.");
+  }
+
+  if (errs.length != 0) {
+    formErr(errs);
+  } else {
+    document.getElementById("regiForm").submit();
+  }
+}
+
+function formErr(errs) {
+  let errorList = errs;
+  getEleId("regiOutput").innerHTML = ""
+  for (var i = 0; i < errorList.length; i++) {
+    getEleId("regiOutput").innerHTML += errorList[i] + "<br>";
+  }
+}
+
+function checkBoxRoundUp() {
+  let usTour = getEleName("usTour[]");
+  let europeTour = getEleName("europeTour[]");
+  let tourCounter = 0;
+
+  for (var i = 0; i < usTour.length; i++) {
+    if (usTour[i].checked) {
+      tourCounter++
+    }
+  }
+
+  for (var i = 0; i < europeTour.length; i++) {
+    if (europeTour[i].checked) {
+      tourCounter++
+    }
+  }
+
+  return tourCounter
+}
